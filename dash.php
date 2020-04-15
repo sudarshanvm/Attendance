c95aae2b527d60ec9efd518143c08b41ce20fc25
Sudarshan VM <sudarshan.vm2000@gmail.com>
2020-04-16 01:02:11 +0530

    final version

--- dash.php
+++ dash.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin- ISE Attendance</title>
+	<title>ISE Attendance</title>
 
 	 <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
